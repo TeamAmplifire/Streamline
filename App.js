1dce13305fc92d008d133721df0a006d72180d02
Chaitanya <chaits98@gmail.com>
2018-07-06 11:23:23 +0530

    Fixed SongList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
